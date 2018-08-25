3f1edb541615161cc548a2081595175d2e90623e
Evan Caldwell <evancald@sas.upenn.edu>
2018-08-25 13:14:37 -0600

    added ESlint airbnb and stopped using quokka in favor of nodemon

--- index.js
+++ index.js
@@ -2,81 +2,77 @@ const axios = require('axios');
 
 
 function getPersonData(personId) {
-return axios.get(`https://swapi.co/api/people/${personId}`)
-  .then((response) => {
-    const person = response.data;
-    
-    const newObj = {
-      name: response.data.name,
-      height: response.data.height,
-      gender: response.data.gender,
-      skinColor: response.data.skin_color,
-      eyeColor: response.data.eye_color,
-      hairColor: response.data.hair_color
-    };
+  return axios.get(`https://swapi.co/api/people/${personId}`)
+    .then((response) => {
+      const person = response.data;
 
-    return newObj;
-  })
-  .catch((error) => {
-    console.log(error.response.status, error.response.statusText);
-  })
+      const newObj = {
+        name: response.data.name,
+        height: response.data.height,
+        gender: response.data.gender,
+        skinColor: response.data.skin_color,
+        eyeColor: response.data.eye_color,
+        hairColor: response.data.hair_color,
+      };
+
+      return newObj;
+    })
+    .catch((error) => {
+      console.log(error.response.status, error.response.statusText);
+    });
 }
 
 const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-Promise.all(ids.map((id) => {
-  return getPersonData(id);
-}))
-.then((dataArr) => {
+Promise.all(ids.map((id) => { return getPersonData(id); }))
+  .then((dataArr) => {
   // We will be working here
-  console.log(dataArr)
+    console.log('Full data array');
+    console.log(dataArr);
 
-  //log a new arr of just the names *hint use .map
-  const namesArr = dataArr.map((person) => {
-    return person.name;
-  });
-  console.log(namesArr);
+    // log a new arr of just the names *hint use .map
+    const namesArr = dataArr.map((person) => person.name);
+    console.log('Names Array');
+    console.log(namesArr);
 
-  //log a new arr, that has all names that include 'b' or 'B' filtered out
-  const noBNames = namesArr.filter((person) => {
-    name = person.toLowerCase();
-    return !(name.includes('b'));
-  }) 
-  console.log(noBNames);
-
-  //log a new arr, with all heights less than 180 filtered out
-  const tallPeople = dataArr.filter((person) => {
-    return person.height > 180;
-  })
-  console.log(tallPeople);
-  
-  //log a new arr, with all items (sorted by name) in (alphabetical order)
-  const alphabetizedNames = namesArr.sort()
-  console.log(alphabetizedNames);
-
-  //log a new arr, with all items (sorted by height) from (least to greatest)
-  const shortestToTallest = dataArr.sort((personA, personB) => {
-    return personA.height - personB.height;
-  })
-  console.log(shortestToTallest);
-
-  //log a statistics object with the following keys and values. Do not include duplicates
-
-  function unique(arr) {
-    let seen = {};
-    return arr.filter((item) => {
-      return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    // log a new arr, that has all names that include 'b' or 'B' filtered out
+    const noBNames = namesArr.filter((person) => {
+      name = person.toLowerCase();
+      return !(name.includes('b'));
     });
-  }
+    console.log('Names with b or B');
+    console.log(noBNames);
 
-  dataArrStats = {
-    count: dataArr.length,
-    names: dataArr.map((person) => {return person.name}),
-    eyeColors: unique(dataArr.map((person) => {return person.eyeColor}).sort()),
-    harColors: unique(dataArr.map((person) => { return person.hairColor}).sort())
-  }
-  console.log(dataArrStats);
+    // log a new arr, with all heights less than 180 filtered out
+    const tallPeople = dataArr.filter((person) => person.height > 180);
+    console.log('Tall people over 180cm');
+    console.log(tallPeople);
 
-})
+    // log a new arr, with all items (sorted by name) in (alphabetical order)
+    const alphabetizedNames = namesArr.sort();
+    console.log('Alphabetized names');
+    console.log(alphabetizedNames);
+
+    // log a new arr, with all items (sorted by height) from (least to greatest)
+    const shortestToTallest = dataArr.sort((personA, personB) => personA.height - personB.height);
+    console.log('Shortest to Tallest');
+    console.log(shortestToTallest);
+
+    // log a statistics object with the following keys and values. Do not include duplicates
+
+    function unique(arr) {
+      const seen = {};
+      return arr.filter((item) => seen.hasOwnProperty(item) ? false : (seen[item] = true));
+    }
+
+    dataArrStats = {
+      count: dataArr.length,
+      names: dataArr.map((person) => person.name),
+      eyeColors: unique(dataArr.map((person) => person.eyeColor).sort()),
+      harColors: unique(dataArr.map((person) => person.hairColor).sort()),
+    };
+    console.log('Data Array Stats');
+    console.log(dataArrStats);
+  });
 
 
 /*
